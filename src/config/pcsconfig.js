730354d73ad7b3609c9fd65b28b2e2cffdd73387
liuzhuoling2011 <liuzhuoling2011@hotmail.com>
2018-10-14 16:56:06 +0800

    修复作为远程机使用时websocket连不上, 导致无法登陆的问题

--- src/config/pcsconfig.js
+++ src/config/pcsconfig.js
@@ -1,8 +1,8 @@
-var port = location.port;
+var host = location.host;
 
 export default {
     base_url: '/', //production
-    ws_url: 'ws://localhost:' + port + '/ws', //production
+    ws_url: 'ws://' + host + '/ws', //production
 
     // base_url: 'http://127.0.0.1:8081/', //develop
     // ws_url: 'ws://127.0.0.1:8081/ws', //develop
